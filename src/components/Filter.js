c8b4769b5f7f7d527e49ec7ea0482c064393c364
Christian <christian.parodi19@gmail.com>
2021-10-21 15:06:18 +0200

    Added 'order by' filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,39 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Filter.css'
+
 import marvel_logo from './../assets/marvel_logo.svg'
 import dc_logo from './../assets/dc_logo.svg'
+
 import ListSubheader from '@mui/material/ListSubheader';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
+import ListItem from '@mui/material/ListItem';
 import Slider from '@mui/material/Slider';
 import Collapse from '@mui/material/Collapse';
-
-import { grey } from '@mui/material/colors';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
 
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import FilterListIcon from '@mui/icons-material/FilterList';
 import CategoryIcon from '@mui/icons-material/Category';
 import EuroIcon from '@mui/icons-material/Euro';
+import ReorderIcon from '@mui/icons-material/Reorder';
 
 export default function Filter() {
-  const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = useState(false);
+    const [selectedMenuIndex, setSelectedMenuIndex] = useState(0)
+    const [anchorEl, setAnchorEl] = useState(null)
+    const [price, setPrice] = React.useState([0, 100]);
 
-  const handleClick = () => {
-    setOpen(open => !open);
-  };
+    const menuOpen = Boolean(anchorEl)
 
-  const [price, setPrice] = React.useState([0, 100]);
+    const menuOptions = [
+        'Ultime uscite',
+        'Prezzo: crescente',
+        'Prezzo: decrescente',
+    ]
 
-  const handlePriceChange = (event, newPrice) => {
-    setPrice(newPrice);
-  };
+    const handleClick = () => {
+        setOpen(open => !open);
+    };
 
-  return (
-      <div className="filter">
-          <List
+    /* Menu interaction */
+    const handleMenuItemClick = (_, index) => {
+        setSelectedMenuIndex(index)
+        setAnchorEl(null)
+    }
+
+    const handleClickMenuListItem = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleCloseMenu = () => {
+        setAnchorEl(null)
+    }
+
+    /* Price slider */
+    const handlePriceChange = (event, newPrice) => {
+        setPrice(newPrice);
+    };
+
+    return (
+        <div className="filter">
+            <List
                 sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
                 component="div"
                 aria-labelledby="nested-list-subheader"
@@ -43,7 +71,53 @@ export default function Filter() {
                         <h3 className="filter-title">Filtri</h3>
                     </ListSubheader>
                 }
+            >
+                {/* Order by */}
+                <List
+                    component="nav"
+                    aria-label="Ordina per"
+                    sx={{ bgcolor: 'background.paper' }}
                 >
+                    <ListItem
+                        button
+                        id="lock-button"
+                        aria-haspopup="listbox"
+                        aria-controls="lock-menu"
+                        aria-label="Ordina per"
+                        aria-expanded={menuOpen ? 'true' : undefined}
+                        onClick={handleClickMenuListItem}
+                    >
+                        <ListItemIcon>
+                            <ReorderIcon />
+                        </ListItemIcon>
+                        <ListItemText
+                            primary="Ordina per"
+                            secondary={menuOptions[selectedMenuIndex]}
+                        />
+                    </ListItem>
+                </List>
+                <Menu
+                    id="lock-menu"
+                    anchorEl={anchorEl}
+                    open={menuOpen}
+                    onClose={handleCloseMenu}
+                    MenuListProps={{
+                        'aria-labelledby': 'lock-button',
+                        role: 'listbox',
+                    }}
+                >
+                    {menuOptions.map((option, index) => (
+                        <MenuItem
+                            key={index}
+                            selected={index === selectedMenuIndex}
+                            onClick={(event) => handleMenuItemClick(event, index)}
+                        >
+                            {option}
+                        </MenuItem>
+                    ))}
+                </Menu>
+                {/* end Order by */}
+                {/* Category */}
                 <ListItemButton onClick={handleClick}>
                     <ListItemIcon>
                         <CategoryIcon />
@@ -53,20 +127,22 @@ export default function Filter() {
                 </ListItemButton>
                 <Collapse in={open} timeout="auto" unmountOnExit>
                     <List component="div" disablePadding>
-                    <ListItemButton sx={{ pl: 10 }}>
-                        <ListItemIcon >
-                            <img src={marvel_logo} alt="Marvel" className="filter-item-logo"/>
-                        </ListItemIcon>
-                        <ListItemText primary="Marvel" />
-                    </ListItemButton>
-                    <ListItemButton sx={{ pl: 10 }}>
-                        <ListItemIcon>
-                            <img src={dc_logo} alt="DC" className="filter-item-logo"/>
-                        </ListItemIcon>
-                        <ListItemText primary="DC Comics" />
-                    </ListItemButton>
+                        <ListItemButton sx={{ pl: 10 }}>
+                            <ListItemIcon >
+                                <img src={marvel_logo} alt="Marvel" className="filter-item-logo" />
+                            </ListItemIcon>
+                            <ListItemText primary="Marvel" />
+                        </ListItemButton>
+                        <ListItemButton sx={{ pl: 10 }}>
+                            <ListItemIcon>
+                                <img src={dc_logo} alt="DC" className="filter-item-logo" />
+                            </ListItemIcon>
+                            <ListItemText primary="DC Comics" />
+                        </ListItemButton>
                     </List>
                 </Collapse>
+                {/* end Category */}
+                {/* Price slider */}
                 <ListItemText>
                     <div className="price-container">
                         <div className="price-filter">
@@ -82,12 +158,13 @@ export default function Filter() {
                                 onChange={handlePriceChange}
                                 valueLabelDisplay="auto"
                                 getAriaValueText={price => `${price}€`}
-                            />                        
+                            />
                         </div>
                     </div>
                 </ListItemText>
+                {/* end Price slider */}
             </List>
-      </div>
-    
-  );
+        </div>
+
+    );
 }
